16ce401d806d9a59673fdbe471667f4f7ea1ae7d
Joao Paulo <jpamdev@gmail.com>
2022-12-04 21:56:28 -0300

    feat: github user search page

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -1,10 +1,30 @@
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import style from "./search.module.scss";
 
 const Search: React.FC = (): JSX.Element => {
+  const navigate = useNavigate();
+  const [inputValue, setInputValue] = useState("");
+
+  const handleKeyPress = (e: { key: string }) => {
+    if (e.key === "Enter") {
+      navigate(`/${inputValue}`);
+    }
+  };
+
   return (
-    <>
-      <p className={style.label}>aghrthe</p>
-    </>
+    <main className={style.main}>
+      <input
+        className={style.input}
+        placeholder="Enter a github username"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+        onKeyPress={handleKeyPress}
+      />
+      <Link className={style.anchor} to={`/${inputValue}`}>
+        Get user details!
+      </Link>
+    </main>
   );
 };
 
