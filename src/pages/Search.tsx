65a121eb0743f2f2b3f51c72a2a99415e471d379
Joao Paulo <jpamdev@gmail.com>
2022-12-04 21:41:06 -0300

    feat: basic application setup

---
+++ src/pages/Search.tsx
@@ -0,0 +1,11 @@
+import style from "./search.module.scss";
+
+const Search: React.FC = (): JSX.Element => {
+  return (
+    <>
+      <p className={style.label}>aghrthe</p>
+    </>
+  );
+};
+
+export default Search;
---
+++ src/pages/UserDetail.tsx
@@ -0,0 +1,11 @@
+import style from "./userDetail.module.scss";
+
+const UserDetail: React.FC = (): JSX.Element => {
+  return (
+    <>
+      <p>a</p>
+    </>
+  );
+};
+
+export default UserDetail;
